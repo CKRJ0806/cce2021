bd31b9974c95d94ebcbed03009169e6cc840a2c2
CKRJ0806 <79628280+CKRJ0806@users.noreply.github.com>
2021-03-25 17:25:12 +0800

    Add files via upload

--- 第五周/0504.cpp
+++ 第五周/0504.cpp
@@ -1,1 +1,10 @@
+#include <stdio.h>
+int main ()
+{
+    char line[5][10]={"decline","proper","majority","bullet","shop"};
+    char *p;
+    for(int i=0;i<5;i++){
 
+        printf("%s\n",line[i]);
+    }
+}
